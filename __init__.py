9d4175c9b0245a31a2dfa6cb55533388365e7413
Ryan Backof <rmbacko92@gmail.com>
2019-01-01 15:48:11 -0500

    Added program scaffolding

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from fictdictCli import *
+from fict_dict.fiction_dictionary import FictionDict 
+from fict_dict.client import CommandLineLib
+from fict_dict.exceptions import DuplicateWord 
+from test.test_client import test_menu 
+from test.fict_dict import test_dictionary
+
+__all__=['fictdictCli','fict_dict.fiction_dictionary', 'fict_dict.client','fict_dict.exceptions']
