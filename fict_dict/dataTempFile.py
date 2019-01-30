639240a1b96a805a6b88be3be49e390f3c7c6756
Ryan Backof <rmbacko92@gmail.com>
2019-01-30 18:54:08 -0500

    Refactored imports, redid createFile test, and createFile function and test to operate correctly

--- fict_dict/dataTempFile.py
+++ fict_dict/dataTempFile.py
@@ -1,5 +1,7 @@
 import os, sys, datetime
+from pathlib import Path
 import json
+from collections import ChainMap
 
 #TODO
 '''
@@ -8,20 +10,20 @@ Implement Functions
 
 class dataFile():
     #directory does not get to be choose by user
-    localCacheDir=os.path.dirname(os.path.abspath(__file__))
+    topLevelDir=Path(os.path.dirname(os.path.abspath(__file__))).parent
 
     #context is the current dictionary that the program is writing to 
     def __init__(self):
-        self.temp_file='temp'
-        self.temp_abs=os.path.join(self.localCacheDir, 'temp', self.temp_file)
+        self.temp_file='temp.json'
+        self.temp_abs=os.path.join(self.topLevelDir, 'dictionaries', self.temp_file)
         self.currentContext={}
 
     def setFileName(self, filename):
        self.temp_file=filename
-       self.temp_abs=os.path.join(self.localCacheDir, 'temp', self.temp_file)
+       self.temp_abs=os.path.join(self.topLevelDir, 'dictionaries', self.temp_file)
     
     #This sould only be done initially. it will overwrite the current context
-    def  setNewDictionary(self, filename, dictionary):
+    def  setNewDictionary(self,  dictionary):
         self.currentContext=dictionary
 
     #Update file dumps the current dictionary the user is writing to a temp file
@@ -32,18 +34,33 @@ class dataFile():
     def  deleteFile(self):
         pass
 
-    #create file creates a new temp file if one does not exist already
+    #create file creates a new temp file if one does not exist, assigns the context dictionary
+    # to the file
+    #if one exist set the context to the first item
     def createFile(self):
+        data_list={1: self.currentContext}
         try:
-            if (os.path.exists(self.temp_abs)):
-                return True
+            if os.path.isfile(self.temp_abs):
+                with open(self.temp_abs, 'r') as f:
+                    data_list=json.load(f)
+                    #Get the first dictionary off the list and set it as the context if the file exist
+                    self.currentContext=next(iter(data_list.values()))
+                    return self.currentContext
             else:
-                with open(self.temp_abs, 'a') as f:
-                    f.close()
-                    return True
-        except:
-            print("IO Error during file creation")
-            return False
+            	raise(FileNotFoundError)
+        except (FileNotFoundError, IOError): 
+            print(": Creating a new temp file: ")
+            try:
+                if  data_list[1]:
+                    with open(self.temp_abs, 'a+') as f:
+                        json.dump(data_list, f)
+                        return self.currentContext
+                else:
+                	raise(ValueError)
+            except ValueError:
+            	print("No default dictionary context was set create a populated dictionary first")
+            	return { 0 :["No default dictionary context was set create a populated dictionary first"]}
+
     
     #print dictionary which is pointed to by key
     def printCurrent(self, key):
--- test/test_temp_file.py
+++ test/test_temp_file.py
@@ -9,19 +9,42 @@ import os
 class test_temp_file(unittest.TestCase):
 
     test_temp=dataFile()
+    testDict={"A": ["The quick brown fox jumps over the lazy dog"]}
 
     def setUp(self):
         pass
 
     def tearDown(self):
-        pass
+        #Remove any temp files remaining after testing
+        if(os.path.isfile(self.test_temp.temp_abs)):
+            os.remove(self.test_temp.temp_abs)
+        self.test_temp.setNewDictionary({})
+
+    def test_setNewDictionary(self):
+        self.test_temp.setNewDictionary(self.testDict)
+        self.assertEqual(self.test_temp.currentContext, self.testDict)
 
     def  test_createFile(self):
         try:
-            result=self.test_temp.createFile()
-            self.assertEqual(os.path.exists(self.test_temp.temp_abs), result)
+            result_dict=self.createFile_Helper()
+            self.assertTrue(os.path.isfile(self.test_temp.temp_abs))
         except:
-            self.fail()
+            self.fail("An unexpected FileError occured")
+
+    def test_createFile_WithFile(self):
+        try:
+            result_dict=self.createFile_Helper()
+            self.assertEqual(result_dict, self.testDict)
+        except:
+            self.fail("An unexpected FileError occured")
+
+    def test_createFile_Errormessage(self):
+        try:
+            result_dict=self.test_temp.createFile()
+            expectedErrorMsg="No default dictionary context was set create a populated dictionary first"
+            self.assertEqual(result_dict[0][0], expectedErrorMsg)
+        except:
+            self.fail("An unexpected FileError occured")
 
     def test_updateFile(self):
         self.fail()
@@ -37,3 +60,8 @@ class test_temp_file(unittest.TestCase):
 
     def test_Contains(self):
         self.fail()
+
+    def createFile_Helper(self):
+        self.test_temp.setNewDictionary(self.testDict)
+        result=self.test_temp.createFile()
+        return result
